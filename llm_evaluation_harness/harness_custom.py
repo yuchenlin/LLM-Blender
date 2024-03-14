0a058fd07ad7df6ddf0ebe545cb5870930fefef8
tonyguo <tony92151@gmail.com>
2024-03-14 23:02:10 +0800

    Refactor imports in harness_custom.py

--- llm_evaluation_harness/harness_custom.py
+++ llm_evaluation_harness/harness_custom.py
@@ -5,13 +5,9 @@ from importlib.util import find_spec
 from typing import List, Literal, Optional, Tuple, Union
 
 from tqdm import tqdm
-
-import lm_eval.models.utils
-from lm_eval import utils
-from lm_eval.api.model import LM, TemplateLM
+from lm_eval.api.model import LM
 from lm_eval.api.registry import register_model
-from lm_eval.models.utils import retry_on_specific_exceptions
-from lm_eval.utils import eval_logger
+
 
 import sys
 sys.path.append('/root/LLM-Blender-harness')

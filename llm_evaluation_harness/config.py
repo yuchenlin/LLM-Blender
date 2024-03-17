847b9f3f82971564174f8fe0d99f8af095fe9e04
tonyguo <tony92151@gmail.com>
2024-03-17 17:00:16 +0800

    update config

--- llm_evaluation_harness/config.py
+++ llm_evaluation_harness/config.py
@@ -4,16 +4,18 @@ supported_model = [
     "databricks/dolly-v2-12b",
     "stabilityai/stablelm-tuned-alpha-7b",
     "OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5",
+
     "TheBloke/koala-13B-HF",
     "project-baize/baize-v2-13b",
     "google/flan-t5-xxl",
-    "THUDM/chatglm-6b",
-    "fnlp/moss-moon-003-sft",
+    "THUDM/chatglm-6b", # x
+    "fnlp/moss-moon-003-sft", # x
+
     "mosaicml/mpt-7b-chat",
-    "TheBloke/guanaco-13B-HF",
-    "NousResearch/Nous-Hermes-13b",
-    "ehartford/WizardLM-13B-Uncensored",
-    "jondurbin/airoboros-13b",
+    # "TheBloke/guanaco-13B-HF",
+    # "NousResearch/Nous-Hermes-13b",
+    # "ehartford/WizardLM-13B-Uncensored",
+    # "jondurbin/airoboros-13b",
 ]
 
 batch_size_map = {

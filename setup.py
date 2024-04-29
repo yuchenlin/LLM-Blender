1317236c923f2cd67b6fad324a65ca4d67adcc78
Dongfu <dongfu.jdf@gmail.com>
2024-04-29 10:45:47 -0400

    updat e

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup, find_packages
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='llm_blender',
     version='0.0.2',
     description='LLM-Blender, an innovative ensembling framework to attain consistently superior performance by leveraging the diverse strengths and weaknesses of multiple open-source large language models (LLMs). LLM-Blender cut the weaknesses through ranking and integrate the strengths through fusing generation to enhance the capability of LLMs.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Dongfu Jiang',
     author_email='dongfu.jdf@gmail.com',
     packages=find_packages(),
